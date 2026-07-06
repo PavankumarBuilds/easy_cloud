0bbd38fdccad4a4b21f8118a959dca41f014f78c
Dinesh-charan-dev <charan@moneybloom.in>
2026-07-06 11:39:51 +0530

    changes are done

--- easy_cloud/api.py
+++ easy_cloud/api.py
@@ -6,3 +6,9 @@ def test_endpoint():
         "status": "success",
         "message": "easy_cloud API is working perfectly!"
     }
+@frappe.whitelist()
+def get_status():
+    return {
+        "server_status": "online",
+        "database": "connected"
+    }
